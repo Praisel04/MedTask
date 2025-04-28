25a8acc7fb57c3cb8855b8a018d2df32a78bfa2c
Ivan Seco <127090971+Praisel04@users.noreply.github.com>
2025-04-28 12:33:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Configuración
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
 app.get('/', (req, res) => res.render('index'));
 app.get('/login', (req, res) => res.render('login'));
 app.get('/menu', (req, res) => res.render('menu'));
 app.get('/privacidad', (req, res) => res.render('privacidad'));
 app.get('/triaje', (req, res) => res.render('triaje'));
-app.get('/hospitales', (req, res) => res.render('hospital'));
+app.get('/hospitales', (req, res) => res.render('hospitales'));
 
-// Exportar la app para Vercel
 module.exports = app;
